8b4dd22f0938179bb34c496fdb337757fc581f0c
Emil Johansson <Emilaik@gmail.com>
2025-06-02 21:49:13 +0200

    Implement UserContext with UserProvider and custom hook

---
+++ Monster-Team-Purple/src/context/UserCOntext.tsx
@@ -0,0 +1,42 @@
+// context/UserContext.tsx
+import React, { createContext, useContext, useState } from "react";
+import type { User } from "../types/UserType";
+
+
+interface UserContextType {
+  users: User[];
+  currentUser: User | null;
+  setCurrentUser: (user: User) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [users] = useState<User[]>([
+    {
+      id: "1",
+      name: "Monster",
+    },
+    {
+      id: "2",
+      name: "MonsterReact",
+    },
+  ]);
+
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={{ users, currentUser, setCurrentUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (!context)
+    throw new Error("useUserContext must be used within UserProvider");
+  return context;
+};
