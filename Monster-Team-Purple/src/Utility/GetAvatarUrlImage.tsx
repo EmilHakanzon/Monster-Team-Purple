21547851e41d6793048e0a5fc968da0e27df3a5c
Emil Johansson <Emilaik@gmail.com>
2025-06-03 21:09:51 +0200

    Revert "Skapa en utility funtion från Sanias logik med bilderna."
    This reverts commit 4deda507f9747bf3824eaf6333de54bd1bafdfc3.

--- Monster-Team-Purple/src/Utility/GetAvatarUrlImage.tsx
+++
@@ -1,7 +0,0 @@
-// Av Sanias logik för att matcha user id med med url så har ja gjort en liten utility funktion.
-// Som används i båda filerna för att få samma bild på user samt att
-//  filerna är mer dynamiska efter ändrad id på user eller ny user så får båda kompunenterna samma bild.
-
-export function getAvatarUrl(userId: string) {
-  return `https://i.pravatar.cc/150?u=${userId}`;
-}
