3178161b9862c83c4aeb59bd8221ecb0c35df517
Emil Johansson <Emilaik@gmail.com>
2025-06-03 20:39:58 +0200

    Tog bort ena usercardcomp för vi hade två stycken. Sen gjorde ja en ny med userProfilCardComp som är den som visar user i startscreen.

--- Monster-Team-Purple/src/components/ui/UserProfilCardComponent.tsx
+++ Monster-Team-Purple/src/components/ui/UserProfilCardComponent.tsx
@@ -1,15 +1,21 @@
 import type { User } from "@/src/types/UserType";
+import { getAvatarUrl } from "@/src/Utility/GetAvatarUrlImage";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 
-interface Props {
+interface UserCardProps {
   user: User;
   onSelect: (user: User) => void;
 }
 
-export const UserCard: React.FC<Props> = ({ user, onSelect }) => {
+export const UserCard: React.FC<UserCardProps> = ({ user, onSelect }) => {
+
+  // Använder utility-funktion här att få bild med matchad id.
+  const avatarUrl = getAvatarUrl(user.id);
   return (
+
     <TouchableOpacity onPress={() => onSelect(user)} style={styles.card}>
+      <Image source={{ uri: avatarUrl }} style={styles.image} />
       <Text style={styles.name}>{user.name}</Text>
     </TouchableOpacity>
   );
@@ -19,6 +25,7 @@ const styles = StyleSheet.create({
   card: {
     margin: 10,
     alignItems: "center",
+    
   },
   image: {
     width: 80,
