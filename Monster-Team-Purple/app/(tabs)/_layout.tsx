5b5a612ae78358d87011a2d1deb735bac69509ea
Emil Johansson <Emilaik@gmail.com>
2025-06-03 21:07:32 +0200

    Revert "Skapar en backButton från stack och döljer den på startscreen samt att jag döljer hela headern förutom backButton"
    This reverts commit 6c531dbea40dbf023edecab8ec723c4adbec70a8.

--- Monster-Team-Purple/app/(tabs)/_layout.tsx
+++ Monster-Team-Purple/app/(tabs)/_layout.tsx
@@ -1,35 +1,17 @@
-import { Ionicons } from "@expo/vector-icons";
-import { Tabs, useRouter } from "expo-router";
+import { Tabs } from "expo-router";
 import React from "react";
-import { Platform, TouchableOpacity } from "react-native";
+import { Platform } from "react-native";
 
 export default function TabLayout() {
-  const router = useRouter();
-
   return (
     <Tabs
       screenOptions={{
-        headerShown: true,
-        headerStyle: {
-          backgroundColor: "#fff",
-          borderBottomWidth: 1,
-          borderBottomColor: "#e0e0e0",
-          shadowColor: "#000",
-          shadowOpacity: 0.05,
-          shadowOffset: { width: 0, height: 2 },
-          shadowRadius: 4,
-          elevation: 2,
-        },
-        headerTitleAlign: "center",
-        headerTitleStyle: {
-          fontWeight: "bold",
-          fontSize: 22,
-          color: "#22223b",
-          letterSpacing: 1,
-        },
-        headerTintColor: "#4B5563",
+        headerShown: false,
         tabBarStyle: Platform.select({
-          ios: { position: "absolute" },
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
           default: {},
         }),
       }}
@@ -37,29 +19,13 @@ export default function TabLayout() {
       <Tabs.Screen
         name="StartScreen"
         options={{
-          headerShown: false, // Döljer headern på StartScreen
+          title: "StartScreen",
         }}
       />
       <Tabs.Screen
-        name="homePage"
-        options={{
-          headerTitle: "",
-          headerStyle: {
-            backgroundColor: "transparent",
-            borderBottomWidth: 0,
-            shadowColor: "transparent",
-            elevation: 0,
-          },
-          headerShadowVisible: false, // tar bort linjen från header.
-          headerLeft: () => (
-            <TouchableOpacity
-              onPress={() => router.back()}
-            >
-              <Ionicons name="arrow-back" size={35} color="#222" />
-            </TouchableOpacity>
-          ),
-        }}
-      />
+        name="HomePage"
+        options={{ title: "Homepage" }}
+      ></Tabs.Screen>
     </Tabs>
   );
 }
